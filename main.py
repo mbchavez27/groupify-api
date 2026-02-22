5d5b213b033d1571127987ccc68c64efdc3d82d2
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-23 01:41:05 +0800

    feat: add the clustering

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from services.ml_model import (
 from services.matchmaker import assign_members
 
 
-def run_pipeline(csv_path: str, text_column: str, num_houses: int):
+def run_pipeline(csv_path: str, text_column: str, num_clusters: int):
     print("1. Initializing NLTK...")
     setup_nltk()
 
@@ -18,23 +18,23 @@ def run_pipeline(csv_path: str, text_column: str, num_houses: int):
     print("3. Cleaning text and extracting keywords...")
     processed_df, unique_keywords_df = prepare_dataframe(dataset, text_column)
 
-    print(f"4. Clustering unique words into {num_houses} houses...")
+    print(f"4. Clustering unique words into {num_clusters} clusters...")
     kmeans, word_embeddings, clustered_words_df = cluster_unique_words(
-        unique_keywords_df, num_houses
+        unique_keywords_df, num_clusters
     )
 
-    print("5. Generating house labels...")
+    print("5. Generating cluster labels...")
     house_labels = get_top_keywords_per_cluster(
         kmeans, word_embeddings, clustered_words_df
     )
     for cluster_id, words in house_labels.items():
-        print(f"   House {cluster_id}: {', '.join(words)}")
+        print(f"   Cluster {cluster_id}: {', '.join(words)}")
 
-    print("6. Assigning members to houses (Load Balancing)...")
+    print("6. Assigning members to clusters (Load Balancing)...")
     final_df = assign_members(processed_df, embedding_model, kmeans, max_diff=20)
 
     # Add the descriptive labels back into the dataframe for easy reading
-    final_df["house_label"] = final_df["assigned_cluster"].apply(
+    final_df["cluster_label"] = final_df["assigned_cluster"].apply(
         lambda x: ", ".join(house_labels.get(x, []))
     )
 
@@ -47,6 +47,6 @@ def run_pipeline(csv_path: str, text_column: str, num_houses: int):
 if __name__ == "__main__":
     TARGET_CSV = "data/" + "lscs_members.csv"
     COLUMN_NAME = "interests"
-    NUMBER_OF_HOUSES = 10
+    NUMBER_OF_CLUSTERS = 5
 
-    run_pipeline(TARGET_CSV, COLUMN_NAME, NUMBER_OF_HOUSES)
+    run_pipeline(TARGET_CSV, COLUMN_NAME, NUMBER_OF_CLUSTERS)
